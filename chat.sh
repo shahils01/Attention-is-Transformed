111986dda1909b661446a6c5a0092145d83ffe82
shahils01 <shahil.shaik7@gmail.com>
2026-06-14 02:31:56 -0400

    no major changes

--- chat.sh
+++ chat.sh
@@ -1,5 +1,5 @@
 python experiments/chat_tinystories.py \
-  --checkpoint /scratch/shahils/lgma_runs/large_tinystories_lgma_residual_multibase_b2_h16/checkpoint_step_175000.pt \
+  --checkpoint /scratch/shahils/lgma_runs/large_tinystories_lgma_residual_multibase_b2_h16/checkpoint_step_210000.pt \
   --data_path /scratch/shahils/lgma_data/tinystory/TinyStoriesV2-GPT4-train.txt \
   --val_data_path /scratch/shahils/lgma_data/tinystory/TinyStoriesV2-GPT4-valid.txt \
   --device cuda:0 \
